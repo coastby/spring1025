35de2a1a287ff2cf2b55fea327bdfc64e4dacfb1
JO YEJI <whdpwl2@gmail.com>
2022-10-25 14:26:14 +0900

    JdbcContext class로 분리

--- src/main/java/com/db/dao/UserDao.java
+++ src/main/java/com/db/dao/UserDao.java
@@ -9,41 +9,17 @@ import java.util.Map;
 
 public class UserDao {
     private DataSource dataSource;
+    private JdbcContext jdbcContext;
 
     public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
+        this.jdbcContext = new JdbcContext(dataSource);
     }
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy smst) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
 
-        try {
-            c = dataSource.getConnection();
-
-            ps = smst.makeStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if(ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if(c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-    }
 
     public void add(final User user) throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement(
@@ -101,7 +77,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException{
-        jdbcContextWithStatementStrategy((c) -> {
+        jdbcContext.workWithStatementStrategy((c) -> {
             return c.prepareStatement("DELETE FROM Users;");
         });
     }
